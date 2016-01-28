423d47ac2d596fca47c0981a1f58df03a286c891
PaulMASNY <paul.masny@securesafety.org>
2016-01-28 13:41:36 +0100

    best and rankhospital correct

--- best.R
+++ best.R
@@ -1,18 +1,27 @@
 best<-function(state = "TX", outcome = "heart attack"){
-options(warn = -1)
+
 o<-c();st<-state;out<-outcome;library(data.table);
 nm<-c("Hospital","State","heart attack","heart failure","pneumonia")
-	if(out %in% nm == F) { stop("invalid outcome")}
+
+# variable setup :
+if(out==nm[3]){out="heart_attack"}
+if(out==nm[4]){out="heart_failure"}
+# Invalid outcome:
+	if(outcome %in% nm == F) { stop("invalid outcome")}
 		else{
-H<-na.omit(fread("~/R_Programming/Assignment3/outcome-of-care-measures.csv",select=c(2,7,11,17,23),na.strings=c("NA","Not Available")))
+H<-fread("~/R_Programming/Assignment3/outcome-of-care-measures.csv",select=c(2,7,11,17,23),na.strings=c("NA","Not Available"))
 names(H)<-c("Hospital","State","heart_attack","heart_failure","pneumonia")
 K<- H[, heart_failure:=as.numeric(heart_failure)];K<- H[, heart_attack:=as.numeric(heart_attack)];K<- H[, pneumonia:=as.numeric(pneumonia)]
-names(H)<-nm
+# Invalid state:
 	if(st %in% unique(K$State) == F) { stop("invalid state")}
 		else{
-setkeyv(K,outcome)
-K<-head(K[State==state,mult="first"],1)
-o<-unlist(K[1,1,with=F])
+setkeyv(K,out)
+K<-K[State==state,mult="first"]
+
+# final subset and writing of a control file :
+K<-na.omit(subset(K,select=c("Hospital","State",paste(out))))
+write.table(K,file="Kframe.txt")
+o<-K[1,1,with=F]
 		    }
 		    }
 return(o[[1]])
--- rankhospital.R
+++ rankhospital.R
@@ -9,23 +9,26 @@ if(out==nm[4]){out="heart_failure"}
 	if(outcome %in% nm == F) { stop("invalid outcome")}
 		else{
 # load procedure for a simplified data.frame :
-H<-na.omit(fread("~/R_Programming/Assignment3/outcome-of-care-measures.csv",select=c(2,7,11,17,23),na.strings=c("NA","Not Available")))
+H<-fread("~/R_Programming/Assignment3/outcome-of-care-measures.csv",select=c(2,7,11,17,23),na.strings=c("NA","Not Available"))
 names(H)<-c("Hospital","State","heart_attack","heart_failure","pneumonia")
 K<- H[, heart_failure:=as.numeric(heart_failure)];K<- H[, heart_attack:=as.numeric(heart_attack)];K<- H[, pneumonia:=as.numeric(pneumonia)]
 #Invalid state procedure :
 	if(st %in% unique(K$State) == F) { stop("invalid state")}
 		else{
 # Sets the key as the outcome variable :
+K<-na.omit(subset(K,select=c("Hospital","State",paste(out))));
 setkeyv(K,out)
+
 # Best selection :
 	if(nb =="best") {K<-head(K[State==st,mult="first"],1); o<-K[1,1,with=F] }
 # Worst selection :
 		else if(nb =="worst"){K<-tail(K[State==st,mult="last"],1);o<-K[1,1,with=F]}
 # Numerically determined selection :
-			else {K<-K[State==st,mult="first"];m<-K[as.numeric(paste(nb)),paste(out),with=F]
+			else {K<-K[State==st,mult="first"];m<-K[as.numeric(paste(nb)),paste(out),with=F];write.table(K,file="Kframe.txt")
 # Multiple results checking and last hospital name output ordering procedure :
 			      n<-K[m]
-			      setkey(n,Hospital); setorder(n,-Hospital,State,heart_attack,heart_failure,pneumonia)
+			      print(n)
+			      n[order(Hospital)]
 			      o<-n[1,1,with=F]      
 }
 }
